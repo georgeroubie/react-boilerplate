abb1c2e0d7095be6df61b1c8b6f3275e70aec464
George Roubie <george.roubie@gmail.com>
2022-01-30 15:12:37 +0200

    Add the animations in the global css

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -3,25 +3,16 @@ import logo from './../../assets/img/logo.svg';
 import styled from 'styled-components';
 
 const Image = styled.img`
-  height: 40vmin;
-  pointer-events: none;
+	height: 40vmin;
+	pointer-events: none;
 
-  @media (prefers-reduced-motion: no-preference) {
-    & {
-      animation: logo-spin infinite 20s linear;
-    }
-  }
-
-  @keyframes logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
+	@media (prefers-reduced-motion: no-preference) {
+		animation: spin infinite 20s linear;
+	}
 `;
 
-export default function Logo() {
-  return <Image src={logo} alt="logo" />;
-}
+const Logo = () => {
+	return <Image src={logo} alt="logo" />;
+};
+
+export default Logo;
