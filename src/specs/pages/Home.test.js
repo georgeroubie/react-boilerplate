61ebe933bfcd112446f93d58f0cae475b2e0c180
georgeroubie <george.roubie@gmail.com>
2022-03-07 02:09:42 +0200

    Remove app folder in specs

--- src/specs/pages/Home.test.js
+++ src/specs/pages/Home.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../../app/pages/Home';
+import Home from '../../app/pages/Home';
 
 test('renders title', () => {
   render(<Home />);
