2f1c1f7d4aac8a52ecc718622f7a6575910b333c
georgeroubie <george.roubie@gmail.com>
2022-03-17 22:10:20 +0200

    Fix typography

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -3,9 +3,7 @@ import { css } from 'styled-components';
 const typography = css`
   body {
     font-family: Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
+    font-size: 16px;
     color: ${({ theme: { colors } }) => colors.textPrimary};
   }
 
