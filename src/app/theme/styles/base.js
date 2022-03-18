6796533640aef00a397842217a17d68423da8b47
georgeroubie <george.roubie@gmail.com>
2022-03-18 13:58:35 +0200

    Add box sizing border-box for all elements

--- src/app/theme/styles/base.js
+++ src/app/theme/styles/base.js
@@ -1,6 +1,13 @@
 import { css } from 'styled-components';
 
 const base = css`
+  *,
+  :before,
+  :after {
+    box-sizing: border-box;
+  }
+
+  html,
   body {
     background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
   }
