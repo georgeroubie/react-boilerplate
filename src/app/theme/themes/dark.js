186c191acc45b778161acaff5d6625f8eba8b11e
georgeroubie <george.roubie@gmail.com>
2022-03-23 13:21:07 +0200

    Update theme

--- src/app/theme/themes/dark.js
+++ src/app/theme/themes/dark.js
@@ -4,10 +4,20 @@ const darkTheme = {
   colors: {
     backgroundPrimary: '#212121',
     backgroundSecondary: '#171717',
+    borderPrimary: '#000000',
     textPrimary: '#dfdfdf',
-    successPrimary: '#00bc8b',
-    warningPrimary: '#ffc200',
-    errorPrimary: '#ff606A',
+    successBackgroundPrimary: '#00bc8b',
+    warningBackgroundPrimary: '#ffc200',
+    warningTextPrimary: '#000000',
+    dangerBackgroundPrimary: '#ff606A',
+    dangerTextPrimary: '#dfdfdf',
+    dangerBackgroundSecondary: '#fa747c',
+  },
+  spacing: {
+    small: '8px',
+    normal: '15px',
+    large: '30px',
+    xlarge: '45px',
   },
 };
 
--- src/app/theme/themes/light.js
+++ src/app/theme/themes/light.js
@@ -3,11 +3,21 @@ const LIGHT_THEME_KEY = 'light';
 const lightTheme = {
   colors: {
     backgroundPrimary: '#ffffff',
-    backgroundSecondary: '#f7f7f7',
+    backgroundSecondary: '#dedede',
+    borderPrimary: '#000000',
     textPrimary: '#333333',
-    successPrimary: '#00bc8b',
-    warningPrimary: '#ffc200',
-    errorPrimary: '#ed2e2e',
+    successBackgroundPrimary: '#00bc8b',
+    warningBackgroundPrimary: '#ffc200',
+    warningTextPrimary: '#333333',
+    dangerBackgroundPrimary: '#ed2e2e',
+    dangerTextPrimary: '#ffffff',
+    dangerBackgroundSecondary: '#ea3d3d',
+  },
+  spacing: {
+    small: '8px',
+    normal: '15px',
+    large: '30px',
+    xlarge: '45px',
   },
 };
 
