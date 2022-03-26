5183be276888407fdde240b94b0b1bdd531fbedf
georgeroubie <george.roubie@gmail.com>
2022-03-26 15:08:09 +0200

    Add font smoothing

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -3,6 +3,8 @@ import { css } from 'styled-components';
 const typography = css`
   body {
     font-family: Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     font-size: 16px;
     color: ${({ theme: { colors } }) => colors.textPrimary};
   }
