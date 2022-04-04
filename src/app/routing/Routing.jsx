c5640bc5d6ad911f646e48b3c742ff58abf863d2
georgeroubie <george.roubie@gmail.com>
2022-04-04 18:36:46 +0300

    Lazy load components

--- src/app/routing/Routing.jsx
+++ src/app/routing/Routing.jsx
@@ -1,15 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import About from '../pages/About';
 import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
-import Settings from '../pages/Settings';
+
+// Lazy load components
+const About = lazy(() => import('../pages/About'));
+const Settings = lazy(() => import('../pages/Settings'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+
+const Loader = ({ children }) => <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
 
 const Routing = () => (
   <Routes>
-    <Route path="*" element={<NotFound />} />
     <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/settings" element={<Settings />} />
+    <Route
+      path="/about"
+      element={
+        <Loader>
+          <About />
+        </Loader>
+      }
+    />
+    <Route
+      path="/settings"
+      element={
+        <Loader>
+          <Settings />
+        </Loader>
+      }
+    />
+    <Route
+      path="*"
+      element={
+        <Loader>
+          <NotFound />
+        </Loader>
+      }
+    />
   </Routes>
 );
 
