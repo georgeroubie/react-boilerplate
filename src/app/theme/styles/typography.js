7ceab2865a8577041dc73d292099a9611befc4fb
georgeroubie <george.roubie@gmail.com>
2022-08-27 12:24:38 +0300

    Respect font size from user settings

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -5,7 +5,8 @@ const typography = css`
     font-family: Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    font-size: 16px;
+    font-size: 1rem;
+    font-weight: ${({ theme: { fontWeight } }) => fontWeight.normal};
     line-height: 1.4rem;
     letter-spacing: 0.05rem;
     color: ${({ theme: { colors } }) => colors.textPrimary};
