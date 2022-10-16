c34bed69c71729247e24f4afc4934197aa0c3581
George Roubie <george.roubie@gmail.com>
2022-10-16 14:55:18 +0300

    Update Wrapper.jsx

--- src/specs/Wrapper.jsx
+++ src/specs/Wrapper.jsx
@@ -3,13 +3,15 @@ import { HashRouter } from 'react-router-dom';
 import { AppProvider } from '../app/state/Context';
 import Theme from '../app/theme/Theme';
 
-const TestWrapper = ({ children }) => (
-  <HashRouter>
-    <AppProvider>
-      <Theme>{children}</Theme>
-    </AppProvider>
-  </HashRouter>
-);
+const TestWrapper = ({ children }) => {
+  return (
+    <HashRouter>
+      <AppProvider>
+        <Theme>{children}</Theme>
+      </AppProvider>
+    </HashRouter>
+  );
+};
 
 TestWrapper.propTypes = {
   children: PropTypes.node.isRequired,
