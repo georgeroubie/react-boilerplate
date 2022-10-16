435c95881fde2787386809c187cc8c23dcbb7ace
George Roubie <george.roubie@gmail.com>
2022-10-16 14:54:58 +0300

    Update index.jsx

--- src/app/pages/not-found/index.jsx
+++ src/app/pages/not-found/index.jsx
@@ -3,14 +3,16 @@ import PageWrapper from '../../components/layout/PageWrapper';
 import Description from '../../components/typography/Description';
 import Title from '../../components/typography/Title';
 
-const NotFound = () => (
-  <PageWrapper>
-    <Title>Oops :(</Title>
-    <Description>The page you are looking for does not exist!</Description>
-    <Description>
-      Click <Link to="/">here</Link> to go to home page.
-    </Description>
-  </PageWrapper>
-);
+const NotFound = () => {
+  return (
+    <PageWrapper>
+      <Title>Oops :(</Title>
+      <Description>The page you are looking for does not exist!</Description>
+      <Description>
+        Click <Link to="/">here</Link> to go to home page.
+      </Description>
+    </PageWrapper>
+  );
+};
 
 export default NotFound;
