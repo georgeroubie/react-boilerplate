b5e239a6164bb911887c81a0d90add39ee643e41
George Roubie <george.roubie@gmail.com>
2022-10-16 14:55:01 +0300

    Update Loader.jsx

--- src/app/routing/Loader.jsx
+++ src/app/routing/Loader.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import { Suspense } from 'react';
 import Description from '../components/typography/Description';
 
-const RoutingLoader = ({ children }) => (
-  <Suspense fallback={<Description>Loading...</Description>}>{children}</Suspense>
-);
+const RoutingLoader = ({ children }) => {
+  return <Suspense fallback={<Description>Loading...</Description>}>{children}</Suspense>;
+};
 
 RoutingLoader.propTypes = {
   children: PropTypes.node.isRequired,
