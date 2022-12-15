c7f9cdbb70966232397b3e42f3a3cd77c4013c2b
George Roubie <george.roubie@gmail.com>
2022-12-15 17:00:36 +0200

    Create IconGenerator.jsx

---
+++ src/app/components/icons/IconGenerator.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const IconGenerator = ({ className, viewBox, width, height, children }) => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      className={className}
+      width={width}
+      height={height}
+    >
+      {children}
+    </svg>
+  );
+};
+
+IconGenerator.propTypes = {
+  className: PropTypes.string.isRequired,
+  viewBox: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default IconGenerator;
