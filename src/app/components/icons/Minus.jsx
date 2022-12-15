82564946de143989204d78644683787feaab0e4b
George Roubie <george.roubie@gmail.com>
2022-12-15 17:00:44 +0200

    Create Minus.jsx

---
+++ src/app/components/icons/Minus.jsx
@@ -0,0 +1,12 @@
+import IconGenerator from './IconGenerator';
+
+const Minus = ({ className, width, height }) => (
+  <IconGenerator className={className} width={width} height={height} viewBox="0 0 384 512">
+    <path
+      fill="currentColor"
+      d="M376 232H8c-4.42 0-8 3.58-8 8v32c0 4.42 3.58 8 8 8h368c4.42 0 8-3.58 8-8v-32c0-4.42-3.58-8-8-8z"
+    />
+  </IconGenerator>
+);
+
+export default Minus;
