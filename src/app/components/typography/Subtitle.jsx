e1cfeda037b17ee73d298b3cac9d489ae9097303
George Roubie <george.roubie@gmail.com>
2022-12-15 15:51:08 +0200

    Create Subtitle.jsx

---
+++ src/app/components/typography/Subtitle.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.h2`
+  font-size: ${({ theme: { fontSize } }) => fontSize.large};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.large};
+  font-weight: ${({ theme: { fontWeight } }) => fontWeight.bold};
+  margin: 0 0 ${({ theme: { spacing } }) => spacing.large};
+`;
+
+const Subtitle = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+Subtitle.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Subtitle;
