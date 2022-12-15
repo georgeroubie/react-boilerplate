7b8aebab31769deb995fc5eea36086f88ef13d74
George Roubie <george.roubie@gmail.com>
2022-12-15 15:51:30 +0200

    Update light.js

--- src/app/theme/themes/light.js
+++ src/app/theme/themes/light.js
@@ -2,20 +2,60 @@ import baseTheme from './base';
 
 const LIGHT_THEME_KEY = 'light';
 
-const lightTheme = {
-  ...baseTheme,
-  colors: {
-    backgroundPrimary: '#ffffff',
-    backgroundSecondary: '#dedede',
-    borderPrimary: '#000000',
-    textPrimary: '#333333',
-    successBackgroundPrimary: '#00bc8b',
-    warningBackgroundPrimary: '#ffc200',
-    warningTextPrimary: '#333333',
-    dangerBackgroundPrimary: '#ed2e2e',
-    dangerTextPrimary: '#ffffff',
-    dangerBackgroundSecondary: '#ea3d3d',
-  },
+const lightThemeColorValues = {
+  blue: '#266dd3',
+  blueLight: '#e1f2ff',
+
+  green: '#00bc8b',
+  greenLight: '#edf7ee',
+
+  yellow: '#ffc200',
+  yellowLight: '#fffcdc',
+
+  red: '#ed2e2e',
+  redLight: '#fff5f3',
+
+  black: '#333333',
+
+  grey: '#9b9b9b',
+  greyLight: '#dfdfdf',
+  greyLighter: '#f7f7f7',
+
+  white: '#ffffff',
 };
 
-export { lightTheme, LIGHT_THEME_KEY };
+const colors = {
+  background: lightThemeColorValues.greyLighter,
+  backgroundSecondary: lightThemeColorValues.greyLight,
+  surface: lightThemeColorValues.white,
+  surfaceSecondary: lightThemeColorValues.black,
+  successPrimary: lightThemeColorValues.green,
+  successSecondary: lightThemeColorValues.greenLight,
+  warningPrimary: lightThemeColorValues.yellow,
+  warningSecondary: lightThemeColorValues.yellowLight,
+  errorPrimary: lightThemeColorValues.red,
+  errorSecondary: lightThemeColorValues.redLight,
+  buttonPrimary: lightThemeColorValues.yellow,
+  buttonPrimaryText: lightThemeColorValues.black,
+  buttonSecondary: lightThemeColorValues.greyLighter,
+  buttonSecondaryText: lightThemeColorValues.black,
+  buttonOutline: lightThemeColorValues.black,
+  buttonOutlineText: lightThemeColorValues.black,
+  buttonOutlineSecondary: lightThemeColorValues.white,
+  buttonOutlineSecondaryText: lightThemeColorValues.white,
+  textPrimary: lightThemeColorValues.black,
+  textSecondary: lightThemeColorValues.grey,
+  textDisabled: lightThemeColorValues.greyLight,
+  borderPrimary: lightThemeColorValues.greyLight,
+  borderSecondary: lightThemeColorValues.grey,
+  outline: lightThemeColorValues.yellowLight,
+  backdrop: lightThemeColorValues.black,
+  dividerPrimary: lightThemeColorValues.greyLighter,
+};
+
+const lightTheme = {
+  ...baseTheme,
+  colors,
+};
+
+export { lightTheme, LIGHT_THEME_KEY, lightThemeColorValues };
