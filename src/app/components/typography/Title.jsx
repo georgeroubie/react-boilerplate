3aa9be867f214519cf99b7b619ec03530d02a057
George Roubie <george.roubie@gmail.com>
2022-12-15 15:51:10 +0200

    Update Title.jsx

--- src/app/components/typography/Title.jsx
+++ src/app/components/typography/Title.jsx
@@ -5,7 +5,7 @@ const Wrapper = styled.h1`
   font-size: ${({ theme: { fontSize } }) => fontSize.xlarge};
   line-height: ${({ theme: { lineHeight } }) => lineHeight.xlarge};
   font-weight: ${({ theme: { fontWeight } }) => fontWeight.extraBold};
-  margin: 0 0 ${({ theme: { spacing } }) => spacing.large};
+  margin: 0 0 ${({ theme: { spacing } }) => spacing.xlarge};
 `;
 
 const Title = ({ children }) => {
