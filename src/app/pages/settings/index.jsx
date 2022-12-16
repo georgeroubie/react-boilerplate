23f2e42b1f5448cabefe3db2c7c26b7e64980907
George Roubie <george.roubie@gmail.com>
2022-12-16 19:41:19 +0200

    Update index.jsx

--- src/app/pages/settings/index.jsx
+++ src/app/pages/settings/index.jsx
@@ -1,42 +1,23 @@
 import { useContext } from 'react';
-import styled from 'styled-components';
 import PageWrapper from '../../components/layout/PageWrapper';
 import Title from '../../components/typography/Title';
+import Checkbox from '../../components/ui/Checkbox';
 import { AppContext } from '../../state/Context';
 import { DARK_THEME_KEY } from '../../theme/themes/dark';
 import { LIGHT_THEME_KEY } from '../../theme/themes/light';
 
-const Selection = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Checkbox = styled.input`
-  margin-right: ${({ theme: { spacing } }) => spacing.small};
-  cursor: pointer;
-`;
-
-const Label = styled.label`
-  cursor: pointer;
-`;
-
 const Settings = () => {
   const { state, setTheme } = useContext(AppContext);
   const { theme } = state;
-  const id = 'theme_selection';
 
-  function handleOnChange({ target }) {
-    setTheme(target.checked ? DARK_THEME_KEY : LIGHT_THEME_KEY);
+  function handleOnChange(checked) {
+    setTheme(checked ? DARK_THEME_KEY : LIGHT_THEME_KEY);
   }
 
   return (
     <PageWrapper>
       <Title>Settings</Title>
-      <Selection>
-        <Checkbox type="checkbox" id={id} name={id} checked={theme === DARK_THEME_KEY} onChange={handleOnChange} />
-        <Label htmlFor={id}>Dark theme</Label>
-      </Selection>
+      <Checkbox label="Dark theme" checked={theme === DARK_THEME_KEY} onChange={handleOnChange} />
     </PageWrapper>
   );
 };
