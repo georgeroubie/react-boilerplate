65f1eda69eb67dae0b55b96942d0986672455401
George Roubie <george.roubie@gmail.com>
2022-12-17 04:31:21 +0200

    Update index.jsx

--- src/app/pages/not-found/index.jsx
+++ src/app/pages/not-found/index.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
 import PageWrapper from '../../components/layout/PageWrapper';
 import Description from '../../components/typography/Description';
+import Hyperlink from '../../components/typography/Hyperlink';
 import Title from '../../components/typography/Title';
 
 const NotFound = () => {
@@ -9,7 +9,7 @@ const NotFound = () => {
       <Title>Oops :(</Title>
       <Description>The page you are looking for does not exist!</Description>
       <Description>
-        Click <Link to="/">here</Link> to go to home page.
+        Click <Hyperlink href="/">here</Hyperlink> to go to home page.
       </Description>
     </PageWrapper>
   );
