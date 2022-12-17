fe74f0d980e8fc11ad0f5ea1535ae98a9d79a7d5
George Roubie <george.roubie@gmail.com>
2022-12-17 04:31:23 +0200

    Update Loader.jsx

--- src/app/routing/Loader.jsx
+++ src/app/routing/Loader.jsx
@@ -1,9 +1,33 @@
 import PropTypes from 'prop-types';
 import { Suspense } from 'react';
-import Description from '../components/typography/Description';
+import styled from 'styled-components';
+import _Icons from '../components/icons/Icons';
+import { setAnimation } from '../theme/styles/helpers';
+
+const Wrapper = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100% - 90px);
+`;
+
+const Icons = styled(_Icons)`
+  width: ${({ theme: { fontSize } }) => fontSize.large};
+  ${setAnimation('spin infinite 2s linear')};
+`;
 
 const RoutingLoader = ({ children }) => {
-  return <Suspense fallback={<Description>Loading...</Description>}>{children}</Suspense>;
+  return (
+    <Suspense
+      fallback={
+        <Wrapper>
+          <Icons type="Spinner" />
+        </Wrapper>
+      }
+    >
+      {children}
+    </Suspense>
+  );
 };
 
 RoutingLoader.propTypes = {
