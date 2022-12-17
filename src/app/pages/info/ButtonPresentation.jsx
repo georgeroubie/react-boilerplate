7c4e86b0764bbe27f6e6d4968fa7102985749b59
George Roubie <george.roubie@gmail.com>
2022-12-17 02:50:48 +0200

    Create ButtonPresentation.jsx

---
+++ src/app/pages/info/ButtonPresentation.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Subtitle from '../../components/typography/Subtitle';
+import Button from '../../components/ui/Button';
+
+const Controls = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${({ theme: { spacing } }) => spacing.normal};
+  margin-bottom: ${({ theme: { spacing } }) => spacing.large};
+`;
+
+const ButtonPresentation = () => {
+  const [size, setSize] = useState('normal');
+  const [variation, setVariation] = useState('primary');
+  const [disabled, setDisabled] = useState('no');
+  const [isLoading, setIsLoading] = useState('no');
+
+  return (
+    <>
+      <Subtitle>{'<Button /> '}</Subtitle>
+      <Controls>
+        <label htmlFor="size">Size:</label>
+        <select name="size" id="size" value={size} onChange={({ target }) => setSize(target.value)}>
+          <option value="small">small</option>
+          <option value="normal">normal</option>
+          <option value="large">large</option>
+        </select>
+
+        <label htmlFor="size">Disabled:</label>
+        <select name="disabled" id="disabled" value={disabled} onChange={({ target }) => setDisabled(target.value)}>
+          <option value="no">no</option>
+          <option value="yes">yes</option>
+        </select>
+
+        <label htmlFor="size">Variation:</label>
+        <select name="variation" id="variation" value={variation} onChange={({ target }) => setVariation(target.value)}>
+          <option value="primary">primary</option>
+          <option value="secondary">secondary</option>
+        </select>
+
+        <label htmlFor="loading">Loading:</label>
+        <select name="loading" id="loading" value={isLoading} onChange={({ target }) => setIsLoading(target.value)}>
+          <option value="no">no</option>
+          <option value="yes">yes</option>
+        </select>
+      </Controls>
+      <Button size={size} variation={variation} disabled={disabled === 'yes'} isLoading={isLoading === 'yes'}>
+        This is the button children
+      </Button>
+    </>
+  );
+};
+
+export default ButtonPresentation;
