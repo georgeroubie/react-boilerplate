d26917eb11de6ee7d2f2f6b64b678c18a22b1ade
George Roubie <george.roubie@gmail.com>
2022-12-17 03:32:45 +0200

    Update Subtitle.jsx

--- src/app/components/typography/Subtitle.jsx
+++ src/app/components/typography/Subtitle.jsx
@@ -5,7 +5,7 @@ const Wrapper = styled.h2`
   font-size: ${({ theme: { fontSize } }) => fontSize.large};
   line-height: ${({ theme: { lineHeight } }) => lineHeight.large};
   font-weight: ${({ theme: { fontWeight } }) => fontWeight.bold};
-  margin: 0 0 ${({ theme: { spacing } }) => spacing.large};
+  margin: 0 0 ${({ theme: { spacing } }) => spacing.normal};
 `;
 
 const Subtitle = ({ children }) => {
