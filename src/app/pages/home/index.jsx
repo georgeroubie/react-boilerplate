b014ccb38082f3fdc3a52828ad9da75a7d3dd7bd
George Roubie <george.roubie@gmail.com>
2022-12-17 03:32:54 +0200

    Update index.jsx

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -1,32 +1,48 @@
-import { useContext } from 'react';
-import styled from 'styled-components';
 import PageWrapper from '../../components/layout/PageWrapper';
 import Description from '../../components/typography/Description';
+import Hyperlink from '../../components/typography/Hyperlink';
+import Subtitle from '../../components/typography/Subtitle';
 import Title from '../../components/typography/Title';
-import { AppContext } from '../../state/Context';
-
-const Input = styled.input``;
 
 const Home = () => {
-  const { state, setUserName } = useContext(AppContext);
-  const { userName } = state;
-
-  function handleOnChange({ target }) {
-    setUserName(target.value);
-  }
-
   return (
     <PageWrapper>
-      <Title>Welcome {userName}</Title>
+      <Title>Welcome</Title>
+      <Subtitle>About</Subtitle>
       <Description>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        This is the React boilerplate that I use in every React project as a starting point. Some of the basic features
+        are:
+        <ul>
+          <li>State management with React Context</li>
+          <li>Styled components</li>
+          <li>Light/Dark Theme</li>
+          <li>React Routing</li>
+          <li>Icons</li>
+          <li>Basic UI Components</li>
+          <li>ESLint</li>
+          <li>Testing</li>
+        </ul>
       </Description>
-      <Input type="text" placeholder="Change your name" onChange={handleOnChange} />
+      <Subtitle>Creator</Subtitle>
+      <Description>
+        My name is George Roubie and you can follow me on&nbsp;
+        <Hyperlink href="https://www.linkedin.com/in/georgeroubie" target="_blank">
+          LinkedIn
+        </Hyperlink>
+        ,&nbsp;
+        <Hyperlink href="https://george-roubie.medium.com" target="_blank">
+          Medium
+        </Hyperlink>
+        ,&nbsp;
+        <Hyperlink href="https://codepen.io/georgeroubie" target="_blank">
+          Codepen
+        </Hyperlink>
+        &nbsp;and&nbsp;
+        <Hyperlink href="https://github.com/georgeroubie" target="_blank">
+          GitHub
+        </Hyperlink>
+        .
+      </Description>
     </PageWrapper>
   );
 };
