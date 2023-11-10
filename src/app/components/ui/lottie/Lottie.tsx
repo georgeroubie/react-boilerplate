61100536c16677e300ce1dbd7ab0198e42e21902
George Roubie <george.roubie@gmail.com>
2023-11-11 01:38:44 +0200

    Add useInView hook and rootMargin prop to Lottie

--- src/app/components/ui/lottie/Lottie.tsx
+++ src/app/components/ui/lottie/Lottie.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { useColorTheme, useReducedMotion } from '@hooks';
+
+import { useColorTheme, useInView, useReducedMotion } from '@hooks';
 import c from 'clsx';
 import LottiePlayer from 'lottie-web';
 import { ReactElement, useEffect, useRef, useState } from 'react';
@@ -11,15 +13,17 @@ import { LottieAnimations } from './constants';
 type Props = {
   className?: string;
   name: LottieAnimations;
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   speed?: number;
+  rootMargin?: string;
   centered?: boolean;
   loop?: boolean;
   autoplay?: boolean;
   player?: any;
   onPlay?: () => void;
   onComplete?: () => void;
+  onClick?: () => void;
 };
 
 const Lottie = ({
@@ -31,19 +35,31 @@ const Lottie = ({
   centered = false,
   loop = true,
   autoplay = true,
+  rootMargin = '150px',
   player,
   onPlay,
   onComplete,
+  onClick,
 }: Props): ReactElement => {
   const ref = useRef<any>(null);
   const lottie = useRef<any>(null);
   const [isPlaying, setIsPlaying] = useState(autoplay);
+  const isIntersecting = useInView(ref, { threshold: 1, rootMargin });
+  const [hasIntersect, setHasIntersect] = useState(false);
+
+  useEffect(() => {
+    if (isIntersecting && !hasIntersect) {
+      setHasIntersect(true);
+    }
+  }, [isIntersecting, hasIntersect]);
 
   const colorTheme = useColorTheme();
 
   const reducedMotion = useReducedMotion();
 
-  function onClick() {
+  function handleOnClick() {
+    onClick?.();
+
     if (isPlaying) {
       return;
     }
@@ -54,47 +70,49 @@ const Lottie = ({
   }
 
   useEffect(() => {
-    lottie?.current?.destroy();
+    if (hasIntersect) {
+      lottie?.current?.destroy();
 
-    lottie.current = LottiePlayer.loadAnimation({
-      container: ref.current,
-      renderer: 'svg',
-      loop,
-      autoplay: reducedMotion ? false : autoplay,
-      path: `/animations/${colorTheme}/${name}.json`,
-    });
+      lottie.current = LottiePlayer.loadAnimation({
+        container: ref.current,
+        renderer: 'svg',
+        loop,
+        autoplay: reducedMotion ? false : autoplay,
+        path: `/animations/${colorTheme}/${name}.json`,
+      });
 
-    lottie.current.onComplete = () => {
-      setIsPlaying(false);
-      onComplete?.();
-    };
+      lottie.current.onComplete = () => {
+        setIsPlaying(false);
+        onComplete?.();
+      };
 
-    if (speed) {
-      lottie.current.setSpeed(speed);
-    }
+      if (speed) {
+        lottie.current.setSpeed(speed);
+      }
 
-    if (player) {
-      player.current = lottie.current;
-    }
+      if (player) {
+        player.current = lottie.current;
+      }
 
-    if (reducedMotion) {
-      lottie.current.stop();
+      if (reducedMotion) {
+        lottie.current.stop();
+      }
     }
 
     return () => {
       lottie.current && lottie.current.destroy();
     };
-  }, [autoplay, loop, name, player, speed, colorTheme, reducedMotion, onComplete]);
+  }, [autoplay, loop, name, player, speed, colorTheme, reducedMotion, hasIntersect, onComplete]);
 
   return (
     <div
       ref={ref}
       className={c(centered && styles.centered, className)}
       style={{
-        height: height + 'px',
-        width: width + 'px',
+        height: height ? height + 'px' : 'auto',
+        width: width ? width + 'px' : '100%',
       }}
-      onClick={onClick}
+      onClick={handleOnClick}
     />
   );
 };
