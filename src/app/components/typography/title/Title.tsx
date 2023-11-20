2580e6ec9f5c0c05994091ebb3d23f4d8b6a2504
George Roubie <george.roubie@gmail.com>
2023-11-20 18:31:04 +0200

    Support change page title

--- src/app/components/typography/title/Title.tsx
+++ src/app/components/typography/title/Title.tsx
@@ -1,13 +1,24 @@
+import { CONSTANTS } from 'app/constants';
 import c from 'clsx';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useEffect } from 'react';
 import styles from './Title.module.scss';
 
 type Props = {
   className?: string;
+
+  pageTitle?: string;
   children: ReactNode;
 };
 
-const Title = ({ className, children }: Props): ReactElement => {
+const Title = ({ className, pageTitle, children }: Props): ReactElement => {
+  useEffect(() => {
+    if (pageTitle) {
+      document.title = `${CONSTANTS.APP_NAME} - ${pageTitle}`;
+    } else {
+      document.title = CONSTANTS.APP_NAME;
+    }
+  }, [pageTitle]);
+
   return <h1 className={c(styles.title, className)}>{children}</h1>;
 };
 
---
+++ src/app/constants.ts
@@ -0,0 +1,3 @@
+export const CONSTANTS = Object.freeze({
+  APP_NAME: import.meta.env.VITE_APP_NAME as string,
+});
